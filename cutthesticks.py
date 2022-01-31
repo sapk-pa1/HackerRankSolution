618c726773557eeb8b4fcf4c52e3c4dc78a9bd60
pa1 <sapkotapawan099@gmail.com>
2022-02-01 05:20:51 +0545

    Completed

--- cutthesticks.py
+++ cutthesticks.py
@@ -1,6 +1,3 @@
-from numpy import size
-
-
 # arr =[ 8 ,8 ,14 ,10 ,3 ,5 ,14 ,12]
 # answer is [8, 7, 6, 4, 3, 2]
 arr = [5, 4, 4, 2, 2, 8]
@@ -10,7 +7,7 @@ def cutthesticks(arr):
   while(len(arr)>=1):
     arr= [x-min(arr) for x in arr]
     result.append(len(arr))
-    arr = [x for x in arr if x!=0]
+    arr = [x for x in arr if x!=0] # using list comprehension to remove the all cutted sticks 
     print(arr)
 
 
