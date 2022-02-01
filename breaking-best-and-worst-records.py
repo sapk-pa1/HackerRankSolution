b497689e20673e078d0aa910979eca184010e33c
pa1 <sapkotapawan099@gmail.com>
2022-02-01 12:27:31 +0545

    Completed

--- breaking-best-and-worst-records.py
+++ breaking-best-and-worst-records.py
@@ -1,12 +1,18 @@
-Scores= [12,24, 12, 24]
+l  = '10 5 20 20 4 5 2 25 1'
+scores =list( map(int, l.split()))
+def Result (scores):
+    max_score_beaten = 0
+    min_score_beaten = 0
+    max_value = scores[0]
+    min_value = scores[0]
+    for i in scores:
+        if i < min_value:
+            min_score_beaten += 1
+            min_value = i
+        if i> max_value:
+            max_score_beaten +=1
+            max_value =i
+    return[max_score_beaten, min_score_beaten]
 
-
-def result (Scores):
-    min_score = min(Scores)
-    total = 0
-    
-    for i in  range(len(a)):
-        if(Scores[i]>min_score):
-            max_score= Scores[i]
-            total+=1
-        
+x = Result(scores)
+print(x)
--- test.py
+++
@@ -1,3 +0,0 @@
-a = [12,6,7,9]
-low = min(a)
-print(low)
